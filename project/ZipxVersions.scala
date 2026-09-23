da0d4bd4490d71650082dc39dd21233b50c4c767
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-23 10:03:35 -0500

    Compile on Scala 3.9.
    The sbt 2.1 and zipx 0.11.1 bump left core on 3.3.8 and docs on 3.8.4.

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -11,7 +11,7 @@ import zipx.*
   */
 object MyVersions extends ZipxVersions:
   val sbt: SbtVersion     = SbtVersion("2.1.0-M2")
-  val scala: ScalaVersion = ScalaVersion("3.3.8")
+  val scala: ScalaVersion = ScalaVersion("3.9.0")
 
   val zio             = Lib("dev.zio", "zio", "2.1.26")
   val zioStreams      = zio.mod("zio-streams")
